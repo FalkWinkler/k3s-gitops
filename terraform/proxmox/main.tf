e8848db519e6c3501e34f66d0f6061e6cf63d7cf
Falk Winkler <fwinkler0573@gmail.com>
2023-06-16 06:44:11 +0200

    terraform/proxmox 3 master/ 3 worker

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -3,10 +3,10 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "2.8.0"
+      version = "2.9.10"
     }
-     sops = {
-      source = "carlpett/sops"
+    sops = {
+      source  = "carlpett/sops"
       version = "0.7.1"
     }
   }
@@ -22,8 +22,8 @@ provider "proxmox" {
   pm_password     = var.password
   pm_tls_insecure = true
   pm_parallel     = 1
-  # pm_debug        = true
-  pm_api_token_id = "terraform-prov@pve!mytoken"
+  #pm_debug        = true
+  pm_api_token_id     = "terraform-prov@pve!mytoken"
   pm_api_token_secret = "0bab4ffc-d570-4d0d-83e9-75f5b480e5d2"
 }
 
@@ -32,7 +32,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_master" {
   count       = var.num_k3s_masters
   name        = "k3s-master-${count.index}"
   target_node = var.server_node
-  clone       = var.tamplate_vm_name
+  clone       = var.template_vm_name
   ssh_user    = var.ssh_user
   os_type     = "cloud-init"
   agent       = 1
@@ -45,58 +45,59 @@ resource "proxmox_vm_qemu" "proxmox_vm_master" {
   searchdomain = var.search_domain
   nameserver   = var.nameserver
   sshkeys      = file("~/.ssh/id_ed25519.pub")
-  ciuser        = data.sops_file.cloudinit_secrets.data["cloudinit_username"]
+  ciuser       = data.sops_file.cloudinit_secrets.data["cloudinit_username"]
   cipassword   = data.sops_file.cloudinit_secrets.data["cloudinit_password"]
-  bootdisk = "scsi0"
+  bootdisk     = "scsi0"
+
 
   disk {
-    type = "scsi"
-    size = "12G"
-    ssd = 1
+    type    = "scsi"
+    size    = "25G"
+    ssd     = 1
     storage = "local-zfs"
   }
 
   network {
-      model = "virtio"
-      bridge = "vmbr1"
-    }
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
 
   #sshkeys      = data.sops_file.secrets.data["k8s.ssh_key"]
 }
 
 resource "proxmox_vm_qemu" "proxmox_vm_workers" {
-  count       = var.num_k3s_nodes
-  name        = "k3s-worker-${count.index}"
-  target_node = var.server_node
-  clone       = var.tamplate_vm_name
-  ssh_user    = var.ssh_user
-  os_type     = "cloud-init"
-  agent       = 1
-  memory      = var.num_k3s_nodes_mem
-  cores       = 4
-  vmid        = 310 + count.index
-  ipconfig0 = "ip=192.168.10.9${1 + count.index}/24,gw=192.168.10.1"
+  count        = var.num_k3s_nodes
+  name         = "k3s-worker-${count.index}"
+  target_node  = var.server_node
+  clone        = var.template_vm_name
+  ssh_user     = var.ssh_user
+  os_type      = "cloud-init"
+  agent        = 1
+  memory       = var.num_k3s_nodes_mem
+  cores        = 4
+  vmid         = 310 + count.index
+  ipconfig0    = "ip=192.168.10.9${1 + count.index}/24,gw=192.168.10.1"
   searchdomain = var.search_domain
   nameserver   = var.nameserver
   sshkeys      = file("~/.ssh/id_ed25519.pub")
   ciuser       = data.sops_file.cloudinit_secrets.data["cloudinit_username"]
   cipassword   = data.sops_file.cloudinit_secrets.data["cloudinit_password"]
-  bootdisk      = "scsi0"
+  bootdisk     = "scsi0"
 
   disk {
-    type = "scsi"
-    size = "20G"
-    ssd = 1
+    type    = "scsi"
+    size    = "25G"
+    ssd     = 1
     storage = "local-zfs"
   }
-   disk {
-    type = "scsi"
-    size = "120G"
-    ssd = 1
+  disk {
+    type    = "scsi"
+    size    = "120G"
+    ssd     = 1
     storage = "SAMSUNG_SSD"
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr1"
   }
 
@@ -105,16 +106,16 @@ resource "proxmox_vm_qemu" "proxmox_vm_workers" {
 resource "local_file" "k3s_file" {
   content = templatefile("./templates/k8s.tpl", {
     k3s_masters = tomap({
-      for instance in proxmox_vm_qemu.proxmox_vm_master:
+      for instance in proxmox_vm_qemu.proxmox_vm_master :
       instance.name => instance.default_ipv4_address
     }),
     k3s_workers = tomap({
-    for instance in proxmox_vm_qemu.proxmox_vm_workers:
-    instance.name => instance.default_ipv4_address
+      for instance in proxmox_vm_qemu.proxmox_vm_workers :
+      instance.name => instance.default_ipv4_address
     }),
     ansible_ssh_private_key_file = "~/.ssh/id_ed25519"
-    })
-    filename = "../../ansible/inventory/hosts.yml"
+  })
+  filename = "../../ansible/inventory/hosts.yml"
 }
 
 output "Master-IPS" {
--- terraform/proxmox/vars.tf
+++ terraform/proxmox/vars.tf
@@ -1,34 +1,34 @@
 variable "username" {
   description = "The username for the DB master user"
   type        = string
-  sensitive = false
-  default = "terraform-pro@pvev"
+  sensitive   = false
+  default     = "terraform-pro@pvev"
 }
 variable "password" {
   description = "The password for the DB master user"
   type        = string
-  sensitive = true
-  default = ""
+  sensitive   = true
+  default     = ""
 }
 
 variable "proxmox-host" {
   description = "The proxmox host name"
   type        = string
-  default = "192.168.10.254"
+  default     = "192.168.10.254"
 
 }
 
 variable "nameserver" {
-  type = string
+  type        = string
   description = "die Ip Adresse des Nameserver"
-  default ="192.168.10.1"
+  default     = "192.168.10.1"
 }
 
 
 variable "search_domain" {
-  type = string
+  type        = string
   description = "die Such Domain"
-  default ="home.lan"
+  default     = "home.lan"
 }
 
 variable "pvt_key" {
@@ -36,23 +36,23 @@ variable "pvt_key" {
 }
 
 variable "num_k3s_masters" {
- default = 3
+  default = 3
 }
 
 variable "num_k3s_masters_mem" {
- default = "2048"
+  default = "2048"
 }
 
 variable "num_k3s_nodes" {
- default = 3
+  default = 3
 }
 
 variable "num_k3s_nodes_mem" {
- default = "8192"
+  default = "8192"
 }
 
-variable "tamplate_vm_name" {
- default = "ubuntu-2204-cloudinit-template"
+variable "template_vm_name" {
+  default = "ubuntu-2204-cloudinit-template"
 }
 
 variable "server_node" {
@@ -61,11 +61,11 @@ variable "server_node" {
 
 variable "sshkeys" {
   description = "ssh keys to drop onto each vm"
-  type = string
-  default =""
+  type        = string
+  default     = ""
 }
 variable "ssh_user" {
   description = "user to put ssh keys under"
-  type = string
-  default = "falk"
+  type        = string
+  default     = "falk"
 }
